be2b9ed34da89d4f304a08ad8d9472a0dc043cca
jjesusmp <jjesusmp@gmail.com>
2016-04-06 18:00:04 +0200

    Login Design 1

---
+++ app/src/main/java/com/example/asus/androidscruminftel/AndroidScrumINFTELActivity.java
@@ -0,0 +1,12 @@
+package com.example.asus.androidscruminftel;
+
+import android.app.Application;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+
+
+public class AndroidScrumINFTELActivity extends Application {
+
+    public static GoogleApiClient mGoogleApiClient;
+
+}
