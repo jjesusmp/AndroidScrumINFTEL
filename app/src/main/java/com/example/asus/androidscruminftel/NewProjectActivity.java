7d84d79a7ae3c2845a6ed5bbcfffab39b73162f1
inftel20 <jmuyor@gmail.com>
2016-04-07 21:10:08 +0200

    Jesus j

---
+++ app/src/main/java/com/example/asus/androidscruminftel/NewProjectActivity.java
@@ -0,0 +1,289 @@
+package com.example.asus.androidscruminftel;
+
+import android.os.AsyncTask;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+import model.Project;
+
+public class NewProjectActivity extends AppCompatActivity {
+
+    private Project project;
+    private Spinner spinner1;
+    private TextView textView;
+    private Button btnSubmit;
+    private int nestados = 1;
+    private String[] arrText;
+    private String[] arrTemp;
+    //private ArrayList<String> arrText2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_project);
+
+        arrText = new String[nestados];
+        for(int i=0; i<nestados;i++){
+            arrText[i]= "Estado 1";
+        }
+
+        arrTemp = new String[arrText.length];
+        MyListAdapter myListAdapter = new MyListAdapter();
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(myListAdapter);
+
+
+        project = new Project();
+
+        addListenerOnSpinnerItemSelection();
+        addListenerOnButton();
+
+    }
+
+    public void addListenerOnSpinnerItemSelection() {
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+
+    }
+
+    public void addListenerOnButton() {
+
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+
+
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+
+                System.out.println("seleccionado" + spinner1.getSelectedItem());
+                Toast.makeText(NewProjectActivity.this,
+                        "OnClickListener : " +
+                                "\nSpinner 1 : " + String.valueOf(spinner1.getSelectedItem()),
+                        Toast.LENGTH_SHORT).show();
+
+
+
+                TextInputEditText textName = (TextInputEditText) findViewById(R.id.input_nameProject);
+                project.setName(textName.getText().toString());
+
+                TextInputEditText textDescriptionProduct = (TextInputEditText) findViewById(R.id.input_descriptionProject);
+                project.setDescription(textDescriptionProduct.getText().toString());
+
+                project.setEstados(arrTemp);//arrTemp
+            }
+
+        });
+    }
+
+    public class CustomOnItemSelectedListener implements AdapterView.OnItemSelectedListener {
+
+        public void onItemSelected(AdapterView<?> parent, View view, int pos,long id) {
+            Toast.makeText(parent.getContext(),
+                    "OnItemSelectedListener : " + parent.getItemAtPosition(pos).toString(),
+                    Toast.LENGTH_SHORT).show();
+            nestados=Integer.parseInt(parent.getItemAtPosition(pos).toString());
+            System.out.println("ESTADO"+nestados);
+//            TextView textEmpty = (TextView) findViewById(R.id.input_status2);
+//            if(estados.equals("uno")){
+//                textEmpty.setVisibility(View.INVISIBLE);
+//
+//            }else if(estados.equals("dos")){
+//                textEmpty.setVisibility(View.VISIBLE);
+//                textEmpty.setHint("Estado 2");
+//            }
+            //arrText = new String[]{"Estado 1"};
+            arrText = new String[nestados];
+            for(int i=0; i<nestados;i++){
+                arrText[i]= "Estado " + (i+1);
+                //arrText2.add(i, "Estado " + i);
+
+            }
+
+
+            arrTemp = new String[arrText.length];
+            MyListAdapter myListAdapter = new MyListAdapter();
+            ListView listView = (ListView) findViewById(R.id.listView);
+            listView.setAdapter(myListAdapter);
+
+
+
+
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> arg0) {
+            // TODO Auto-generated method stub
+        }
+
+    }
+
+    private class MyListAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            // TODO Auto-generated method stub
+            if(arrText != null && arrText.length != 0){
+                return arrText.length;
+            }
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            // TODO Auto-generated method stub
+            return arrText[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            // TODO Auto-generated method stub
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            //ViewHolder holder = null;
+            final ViewHolder holder;
+            if (convertView == null) {
+
+                holder = new ViewHolder();
+                LayoutInflater inflater = NewProjectActivity.this.getLayoutInflater();
+                convertView = inflater.inflate(R.layout.listview_list, null);
+                holder.editText1 = (EditText) convertView.findViewById(R.id.editText1);
+
+                convertView.setTag(holder);
+
+            } else {
+
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            holder.ref = position;
+
+            holder.editText1.setText(arrTemp[position]);
+            holder.editText1.setHint(arrText[position]);
+            holder.editText1.addTextChangedListener(new TextWatcher() {
+
+                @Override
+                public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+                    // TODO Auto-generated method stub
+
+                }
+
+                @Override
+                public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+                                              int arg3) {
+                    // TODO Auto-generated method stub
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable arg0) {
+                    // TODO Auto-generated method stub
+                    arrTemp[holder.ref] = arg0.toString();
+                }
+            });
+
+
+            return convertView;
+        }
+
+        private class ViewHolder {
+            TextView textView1;
+            EditText editText1;
+            int ref;
+        }
+
+
+    private class UploadProjectClass extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            try {
+                return uploadProject(urls[0]);
+            } catch (IOException e) {
+
+                return null;
+            }
+
+        }
+
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String json) {
+            // Convert String to json object
+            Toast.makeText(getBaseContext(), "New project", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private String uploadProject(String myurl) throws IOException {
+
+        URL url = new URL("URL!!!!");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(10000 /* milliseconds */);
+        conn.setConnectTimeout(15000 /* milliseconds */);
+        conn.setRequestMethod("POST");
+        conn.setDoInput(true);
+        conn.setDoOutput(true);
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setRequestProperty("Accept", "application/json");
+
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("nombre", project.getName());
+            jsonObject.put("descripcion", project.getDescription());
+            jsonObject.put("id_admin", "jjaldoasenjo@gmail.com");
+            jsonObject.put("estados", project.getEstados());
+            jsonObject.put("chat", "");
+            //jsonObject.put("fecha_inicio")
+            jsonObject.put("id_proyecto", 1);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        OutputStreamWriter wr= new OutputStreamWriter(conn.getOutputStream());
+        wr.write(jsonObject.toString());
+        wr.flush();
+        wr.close();
+
+        // Starts the query
+        conn.connect();
+        conn.getResponseCode();
+
+        return "";
+
+    }
+}
+}
+
--- app/src/main/java/model/Project.java
+++ app/src/main/java/model/Project.java
@@ -1,7 +1,103 @@
 package model;
 
+
+import java.sql.Date;
+import java.util.Arrays;
+
 /**
  * Created by Asus on 06/04/2016.
  */
 public class Project {
+    String name;
+    String description;
+    int idProject;
+    int idAdmin;
+    Date dateStart;
+    String[] chat;
+    String[] estados;
+
+
+
+    public Project(){
+
+    }
+
+    public Project(String name, String description, int idProject, int idAdmin, Date dateStart,String[] chat, String[] estados){
+        this.name = name;
+        this.description = description;
+        this.idProject = idProject;
+        this.idAdmin = idAdmin;
+        this.dateStart = dateStart;
+        this.estados=estados;
+        this.chat=chat;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public int getIdProject (){
+        return idProject;
+    }
+
+    public void setIdProject(int idProject){
+        this.idProject = idProject;
+    }
+
+    public int getIdAdmin() {
+        return idAdmin;
+    }
+
+    public void setIdAdmin(int idAdmin) {
+        this.idAdmin = idAdmin;
+    }
+
+    public Date getDateStart() {
+        return dateStart;
+    }
+
+    public void setDateStart(Date dateStart) {
+        this.dateStart = dateStart;
+    }
+
+    public String[] getChat() {
+        return chat;
+    }
+
+    public void setChat(String[] chat) {
+        this.chat = chat;
+    }
+
+    public String[] getEstados() {
+        return estados;
+    }
+
+    public void setEstados(String[] estados) {
+        this.estados = estados;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", idProject=" + idProject +
+                ", idAdmin=" + idAdmin +
+                ", dateStart=" + dateStart +
+                ", chat=" + Arrays.toString(chat) +
+                ", estados=" + Arrays.toString(estados) +
+                '}';
+    }
 }
