bd91596afb7a11525e6680459edc9eba4dcd12a5
polenoso <aitor.p.n@gmail.com>
2016-04-12 11:03:46 +0200

    Added files via upload

---
+++ app/src/main/java/com.example.asus.androidscruminftel/interfaces/ImageLoaderListener.java
@@ -0,0 +1,10 @@
+package androidscruminftel.interfaces;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by aitorpagan on 10/4/16.
+ */
+public interface ImageLoaderListener {
+    public void onImageLoaded(Drawable drawable);
+}
---
+++ app/src/main/java/com.example.asus.androidscruminftel/interfaces/ServiceListener.java
@@ -0,0 +1,13 @@
+package androidscruminftel.interfaces;
+
+import com.example.asus.androidscruminftel.model.ProjectChat;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Asus on 08/04/2016.
+ */
+public interface ServiceListener {
+    public void onServiceResponse(ArrayList<ProjectChat> response);
+    public void onServiceNoResponse(String result);
+}
